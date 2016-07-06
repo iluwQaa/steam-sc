279967d5bab1cb8489ecf8ed70e7c791c326d827
iluwQaa <mscorec@gmail.com>
2016-07-06 17:31:06 +0300

    steam recomendations auto clicker

---
+++ recomendations-clicker.js
@@ -0,0 +1,7 @@
+$(document).ready(function() { 
+var element = document.getElementById("refresh_queue_btn");
+		if(element && element.click) {
+  			 element.click();
+		};
+	$('#next_in_queue_form').submit();
+});
