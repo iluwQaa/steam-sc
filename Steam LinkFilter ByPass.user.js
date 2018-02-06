e490172bd1ba2349fedac84bc8719b9c6d543eb7
iluwka <iluwQaa@users.noreply.github.com>
2018-02-06 14:47:24 +0200

    new sc

---
+++ Steam LinkFilter ByPass.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Steam LinkFilter ByPass
+// @version      0.1
+// @description  LinkFilter bypass
+// @author       iluwQaa
+// @match        http*://steamcommunity.com/linkfilter/?url=https://www.steamgifts.com/giveaway/*
+// @match        http*://steamcommunity.com/linkfilter/?url=http://www.steamtradematcher.com/*
+// @grant        none
+// @icon         https://store.steampowered.com/favicon.ico
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById('proceedButton').click();
+})();
